ae92b99aca58957956c9222cbf393766acd804da
Chunky <jasonc@nulogy.com>
2020-03-01 21:46:04 -0500

    Document expected output

--- statement.js
+++ statement.js
@@ -70,3 +70,11 @@ const invoices =
 ]
 
 statment(invoices[0], plays);
+
+// Expected Output:
+// Statement for BigCo
+//  Hamlet: $650.00 (55 seats)
+//  As You Like It: $580.00 (35 seats)
+//  Othello: $500.00 (40 seats)
+//  Amount owed is $1,730.00
+//  You earned 47 credits
